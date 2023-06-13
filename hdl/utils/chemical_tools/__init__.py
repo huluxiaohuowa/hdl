561c59aeb1b01946732d933dcc00fe4ffb1761c2
jianxing.hu <jianxing.hu@xtalpi.com>
2023-06-13 14:30:33 +0800

    feat: 🎸 sdf tools

---
+++ hdl/utils/chemical_tools/__init__.py
@@ -0,0 +1,2 @@
+from .sdf import sdf2df
+from query_info import query_a_compound
---
+++ hdl/utils/chemical_tools/sdf.py
@@ -0,0 +1,19 @@
+from rdkit import Chem
+
+
+def sdf2df(
+    sdf_file,
+    id_col: str = 'Molecule Name',
+    target_col: str = 'Average △G (kcal/mol)'
+):
+    supp = Chem.SDMolSupplier(sdf_file)
+    mol_dict_list = []
+    for mol in supp:
+        smiles = Chem.MolToSmiles(mol)
+        mol_dict = mol.GetPropsAsDict()
+        mol_dict['smiles'] = smiles
+        mol_dict_list.append(mol_dict)
+        mol_dict['y'] = mol_dict.pop(target_col)
+        mol_dict['name'] = mol_dict.pop(id_col)
+    df = pd.DataFrame(mol_dict_list)
+    return df

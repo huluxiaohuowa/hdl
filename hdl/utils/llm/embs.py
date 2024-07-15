2445b34d6c7d64b72d6e059c4b2adabf86df0efd
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-07-15 05:18:21 +0000

    feat: 🎸 bce in embeddings

--- hdl/utils/llm/embs.py
+++ hdl/utils/llm/embs.py
@@ -1,23 +1,33 @@
-from FlagEmbedding import BGEM3FlagModel
-
-
 class FlagEmbedder():
     def __init__(
         self,
+        emb_name: str = "bge",
         emb_dir: str = None
     ) -> None:
-        """Initialize the class with the specified embedding directory.
+        """Initializes the object with the specified embedding name and directory.
         
         Args:
-            emb_dir (str): The directory containing the embeddings.
-            
+            emb_name (str): The name of the embedding. Defaults to "bge".
+            emb_dir (str): The directory path for the embedding model.
+        
         Returns:
             None
         """
-        self.model = BGEM3FlagModel(
-            emb_dir,  
-            use_fp16=True
-        )
+        self.emb_name = emb_name
+        self.emb_dir = emb_dir
+
+        if "bge" in emb_name.lower():
+            from FlagEmbedding import BGEM3FlagModel
+            self.model = BGEM3FlagModel(
+                emb_dir,  
+                use_fp16=True
+            )
+        elif "bce" in emb_name.lower():
+            from BCEmbedding import EmbeddingModel
+            self.model = EmbeddingModel(
+                model_name_or_path=emb_dir,
+                use_fp16=True
+            )
     
     def encode(
         self,
@@ -37,6 +47,8 @@ class FlagEmbedder():
             return_sparse=True,
             return_colbert_vecs=False
         )
+        if "bge" in self.emb_name.lower():
+            return output["dense_vecs"]
         return output
     
     def sim(
@@ -55,5 +67,5 @@ class FlagEmbedder():
         """
         output_1 = self.encode(sentences_1)
         output_2 = self.encode(sentences_2)
-        similarity = output_1['dense_vecs'] @ output_2['dense_vecs'].T
+        similarity = output_1 @ output_2.T
         return similarity.item()

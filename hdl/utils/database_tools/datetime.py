ffababac5dd9bdbad2d2351fe9065f054fdf511b
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-08-28 15:00:32 +0800

    feat(utils): add datetime functionality
    Add a new module `datetime.py` under `utils/database_tools` to provide functionality for retrieving current date and time of a city based on its name. The module includes a function `get_datetime_by_cityname(city)` which uses geolocation services like Nominatim and TimezoneFinder to fetch the city's timezone information and current time, returning a formatted date and time string. Updated requirements.txt to include necessary packages for the new functionality.

---
+++ hdl/utils/database_tools/datetime.py
@@ -0,0 +1,34 @@
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import pytz
+
+def get_datetime_by_cityname(city):
+    """Get current date and time of a city based on its name.
+
+    Args:
+        city_name (str): The name of the city to get date and time for.
+
+    Returns:
+        str: Formatted date and time string in the format '%Y年%m月%d日 %H:%M:%S'.
+    """
+    # 使用 Nominatim API 通过城市名称获取地理坐标
+    geolocator = Nominatim(user_agent="city_time_locator")
+    location = geolocator.geocode(city)
+
+    if not location:
+        return f"无法找到城市 '{city}'，请检查输入的城市名称。"
+
+    # 使用地理坐标获取时区
+    tf = TimezoneFinder()
+    timezone_name = tf.timezone_at(lng=location.longitude, lat=location.latitude)
+
+    if not timezone_name:
+        return f"无法找到城市 '{city}' 的时区。"
+
+    # 获取当前时间
+    timezone = pytz.timezone(timezone_name)
+    city_time = datetime.now(timezone)
+
+    # 返回格式化的日期时间字符串
+    return city_time.strftime('%Y年%m月%d日 %H:%M:%S')
--- hdl/utils/desc/func_desc.py
+++ hdl/utils/desc/func_desc.py
@@ -1,10 +1,10 @@
 FN_DESC = {
     "get_weather": """
 ## 函数名：get_weather
-描述：在用户询问天气时，获得一个城市的天气信息
+描述：在用户询问一个城市的天气时，调用此工具获得此城市的天气信息
 参数：
 # city (str): 城市名
-返回值：天气信息
+返回值 (str)：天气信息
 需要返回的json
 {
     "function_name": "get_weather",
@@ -13,6 +13,23 @@ FN_DESC = {
             "city": <city_name>
         }
 }
+
+""",
+    "get_datetime_by_cityname": """
+## 函数名：get_datetime_by_cityname
+描述：在用户询问一个城市当前的日期或时间时，调用此工具可以获得此城市当前的日期和时间
+参数：
+# city (str): 城市名
+返回值 (str):这个城市当前所在时区的日期和时间
+需要返回的json
+{
+    "function_name": "get_datetime_by_cityname",
+    "params":
+        {
+            "city": <city_name>
+        }
+}
+
 """,
     "default": None
 }

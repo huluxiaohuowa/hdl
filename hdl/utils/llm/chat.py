4a2adda737a8e77f618f96961358b41e1b0dad36
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-12-04 17:03:28 +0800

    chore(hdl): fix string formatting issue in chat.py
    Fixes a string formatting issue by correcting the use of quotes for accessing dictionary keys in the `OpenAI_M` class in `chat.py`. The change ensures proper handling of keys within the string interpolation, improving code readability and maintainability.

--- hdl/utils/llm/chat.py
+++ hdl/utils/llm/chat.py
@@ -271,7 +271,8 @@ class OpenAI_M():
                     current_info += f"\n{tool_resp}"
                 else:
                     if step_json.get("stop_thinking", False):
-                        current_info += f"\n{step_json.get("content", "")}"
+                        # current_info += f"\n{step_json.get("content", "")}"
+                        current_info += f"\n{step_json.get('content', '')}"
                         yield n_steps, current_info, steps
                         return
                     # 如果不使用工具，将当前思考步骤的标题累积到当前信息中
